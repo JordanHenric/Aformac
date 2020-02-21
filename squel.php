cd4142054fc6a9fcf6de199cdc4fa121fab87786
JordanHenric <46777284+JordanHenric@users.noreply.github.com>
2020-02-21 14:06:55 +0100

    Add files via upload

---
+++ squel.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="viewport" content="width=device-width, initial-scale, shrink-to-fit=no">
+
+    <title>Accueil</title>
+    <link rel="stylesheet" href="./css/style.css.version=1.0.0">
+</head>
+<body>
+    
+    <header>
+        <nav>
+            <!-- -->
+        </nav>
+    </header>
+
+    <section>
+         <article>
+             <!-- -->
+         </article>
+    </section>
+
+    <aside>
+        <!-- -->
+    </aside>
+
+    <footer>
+        <!-- -->
+    </footer>
+
+</body>
+
+</html>
